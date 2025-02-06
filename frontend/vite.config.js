b9e22ad792cf2aff5845ba8d6bfd03426497da58
zach kramer <zkramer22@gmail.com>
2025-02-06 13:30:21 -0800

    add github workflow for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         vue(),
         svgLoader(),
     ],
+    base: '/',
     resolve: {
         alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
